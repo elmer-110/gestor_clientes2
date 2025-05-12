41bb18063b9add6336106a0d51ab5b9b52fe0676
elmer-110 <ecedillov@student.universidadunie.com>
2025-05-12 11:15:13 +0200

    mejora de menu

--- Gestor_de_clientes/gestor/menu.py
+++ Gestor_de_clientes/gestor/menu.py
@@ -1,39 +1,72 @@
 import os
 import helpers
+import database as db
 helpers.limpiar_pantalla()
 def iniciar():
-     while True:
-        os.system('clear') # cls en Windows
+    while True:
+        helpers.limpiar_pantalla()
+
         print("========================")
-        print(" BIENVENIDO AL Manager ")
+        print("  Bienvenido al Gestor  ")
         print("========================")
-        print("[1] Listar clientes ")
-        print("[2] Buscar cliente ")
-        print("[3] Añadir cliente ")
-        print("[4] Modificar cliente ")
-        print("[5] Borrar cliente ")
-        print("[6] Cerrar el Manager ")
+        print("[1] Listar los clientes ")
+        print("[2] Buscar un cliente   ")
+        print("[3] Añadir un cliente   ")
+        print("[4] Modificar un cliente")
+        print("[5] Borrar un cliente   ")
+        print("[6] Cerrar el Gestor    ")
         print("========================")
+
         opcion = input("> ")
-        os.system('clear') # cls en Windows
+        helpers.limpiar_pantalla()
+
         if opcion == '1':
             print("Listando los clientes...\n")
-        if opcion == '2':
-            print("Buscando un cliente...\n")
-        if opcion == '3':
-            print("Añadiendo un cliente...\n")
-        if opcion == '4':
-            print("Modificando un cliente...\n")
-        if opcion == '5':
-            print("Borrando un cliente...\n")
-        if opcion == '6':
-            print("Saliendo...\n")
-        break
-input("\nPresiona ENTER para continuar...")
+            for cliente in db.Clientes.lista:
+                print(cliente)
 
-def leer_texto(longitud_min=0, longitud_max=100, mensaje=None):
-    print(mensaje) if mensaje else None
-    while True:
-        texto = input("> ")
-        if len(texto) >= longitud_min and len(texto) <= longitud_max:
-            return texto
+        elif opcion == '2':
+            print("Buscando un cliente...\n")
+            dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
+            cliente = db.Clientes.buscar(dni)
+            print(cliente) if cliente else print("Cliente no encontrado.")
+
+        elif opcion == '3':
+            print("Añadiendo un cliente...\n")
+
+            dni = None
+            while True:
+                dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
+                if helpers.dni_valido(dni, db.Clientes.lista):
+                    break
+
+            nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
+            apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
+            db.Clientes.crear(dni, nombre, apellido)
+            print("Cliente añadido correctamente.")
+
+        elif opcion == '4':
+            print("Modificando un cliente...\n")
+            dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
+            cliente = db.Clientes.buscar(dni)
+            if cliente:
+                nombre = helpers.leer_texto(
+                    2, 30, f"Nombre (de 2 a 30 chars) [{cliente.nombre}]").capitalize()
+                apellido = helpers.leer_texto(
+                    2, 30, f"Apellido (de 2 a 30 chars) [{cliente.apellido}]").capitalize()
+                db.Clientes.modificar(cliente.dni, nombre, apellido)
+                print("Cliente modificado correctamente.")
+            else:
+                print("Cliente no encontrado.")
+
+        elif opcion == '5':
+            print("Borrando un cliente...\n")
+            dni = helpers.leer_texto(3, 3, "DNI (2 int y 1 char)").upper()
+            print("Cliente borrado correctamente.") if db.Clientes.borrar(
+                dni) else print("Cliente no encontrado.")
+
+        elif opcion == '6':
+            print("Saliendo...\n")
+            break
+
+        input("\nPresiona ENTER para continuar...")
