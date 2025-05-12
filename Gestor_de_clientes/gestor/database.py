a23c888ec6b9e08093135e713b5e14d2f4f6b8aa
elmer-110 <ecedillov@student.universidadunie.com>
2025-05-12 11:32:40 +0200

    crear, modificar y borrar un cliente

--- Gestor_de_clientes/gestor/database.py
+++ Gestor_de_clientes/gestor/database.py
@@ -23,3 +23,33 @@ class Clientes:
         for cliente in Clientes.lista:
             if cliente.dni == dni:
                 return cliente
+    @staticmethod
+    def crear(dni, nombre, apellido):
+        cliente = Cliente(dni, nombre, apellido)
+        Clientes.lista.append(cliente)
+        Clientes.guardar()
+        return cliente
+
+    @staticmethod
+    def modificar(dni, nombre, apellido):
+        for indice, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                Clientes.lista[indice].nombre = nombre
+                Clientes.lista[indice].apellido = apellido
+                Clientes.guardar()
+                return Clientes.lista[indice]
+
+    @staticmethod
+    def borrar(dni):
+        for indice, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                cliente = Clientes.lista.pop(indice)
+                Clientes.guardar()
+                return cliente
+
+    @staticmethod
+    def guardar():
+        with open(config.DATABASE_PATH, 'w', newline='\n') as fichero:
+            writer = csv.writer(fichero, delimiter=';')
+            for cliente in Clientes.lista:
+                writer.writerow((cliente.dni, cliente.nombre, cliente.apellido))
